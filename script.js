32254487d0649c462f1ff0fcbb9b19e61437a8a9
Isaac Bailey <isaacroybailey@icloud.com>
2024-07-18 16:52:59 +0100

    basic calculator operations finished

--- script.js
+++ script.js
@@ -1,13 +1,23 @@
-var first = new Number();
-var operator = new String(); 
-var second = new Number();
+var first = '';
+var operator = ''; 
+var second = '';
+var output = document.getElementById("screen");
+var displayValue = output.innerHTML;
+var operatorIn = false
+
+var operators = {
+    "+":"add",
+    "-":"subtract",
+    "x":"multiply",
+    "/":"divide"
+}
 
 function add(num1, num2){
-    return num1+num2;
+    return Number(num1)+Number(num2);
 }
 
 function subtract(num1,num2){
-    return num1-num2;
+    return Number(num1)-Number(num2);
 }
 
 function multiply(num1,num2){
@@ -20,4 +30,39 @@ function divide(num1,num2){
 
 function operate(operator,num1,num2){
     return window[operator](num1,num2);
+}
+
+function fill(value){
+    output.textContent += String(value);
+    displayValue += String(value);
+    if (["+","-","x","/"].includes(value)){
+        operatorIn = true
+        operator = operators[value];
+        return
+    }
+    if(!operatorIn){
+        first += String(value);
+    }
+    else {
+        second += String(value);
+    }
+}
+
+function equate(){
+    if(!operatorIn || !second){
+        return
+    }
+    output.textContent = parseFloat(window[operator](first,second).toFixed(3));
+    first = output.textContent
+    operator = '';
+    second = '';
+    operatorIn = false;
+}
+
+function Clear(){
+    output.textContent = '';
+    first = '';
+    operator = '';
+    second = '';
+    operatorIn = false;
 }
